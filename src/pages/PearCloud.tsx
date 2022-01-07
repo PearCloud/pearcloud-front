0ca7e014adb875cd4e366dc28d8dcff18bc28e06
ecandotti <enzo.candotti16@hotmail.com>
2022-01-07 23:41:37 +0100

    app page

---
+++ src/pages/PearCloud.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import { Navbar } from '../components/Navbar'
+
+export const PearCloud: React.FC = () => {
+    return (
+        <div className="bg-cyan-100 dark:bg-cyan-800">
+            <Navbar />
+        </div>
+    )
+}
