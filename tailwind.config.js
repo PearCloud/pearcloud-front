ee918174e05d183b74ba7b6aa23a6f2c0697dafb
ecandotti <enzo.candotti16@hotmail.com>
2022-01-07 01:18:47 +0100

    init tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    fontFamily: {
+      monderatBold: ['"Moderat-Bold"', 'sans-serif'],
+      monderatLight: ['"Moderat-Light"', 'sans-serif'],
+      monderatMedium: ['"Moderat-Medium"', 'sans-serif'],
+      monderatRegular: ['"Moderat-Regular"', 'sans-serif'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
