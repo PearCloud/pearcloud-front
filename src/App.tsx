9128f150cada25ad30b6261e8f38ca30de67157d
ecandotti <enzo.candotti16@hotmail.com>
2022-01-07 23:40:36 +0100

    update skelette

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,17 @@ import { PrivateRoute } from './PrivateRoute'
 
 import { Connexion } from './pages/Connexion'
 import { NotFound } from './pages/NotFound'
-import { Home } from './pages/Home'
-import { Albums } from './pages/Albums'
+import { PearCloud } from './pages/PearCloud'
 
 export const App: React.FC = () => {
     return (
         <Routes>
             <Route path={urls.app.LOGIN} element={<Connexion />} />
             <Route
-                path={urls.app.HOME}
+                path={urls.app.PEARCLOUD}
                 element={
                     <PrivateRoute>
-                        <Home />
-                    </PrivateRoute>
-                }
-            ></Route>
-            <Route
-                path={urls.app.ALBUMS}
-                element={
-                    <PrivateRoute>
-                        <Albums />
+                        <PearCloud />
                     </PrivateRoute>
                 }
             ></Route>
