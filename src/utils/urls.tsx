1d8ee6e4ba6294bf395a7589e0c6855a31a75736
ecandotti <enzo.candotti16@hotmail.com>
2022-01-07 01:23:08 +0100

    const of routes in APP and for API

---
+++ src/utils/urls.tsx
@@ -0,0 +1,20 @@
+export const urls = {
+    app: {
+        LOGIN: '/login',
+        LOGOUT: '/logout',
+        HOME: '/home',
+        FAVORITES: '/favorites',
+        ALBUMS: '/albums',
+        TRASH: '/trash',
+        SETTINGS: '/settings',
+    },
+    api: {
+        LOGIN: '/api/login',
+        LOGOUT: '/api/logout',
+        HOME: '/api/home',
+        FAVORITES: '/api/favorites',
+        ALBUMS: '/api/albums',
+        TRASH: '/api/trash',
+        SETTINGS: '/api/settings',
+    }
+}
