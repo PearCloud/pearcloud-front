7e6bb607fa6c96f3f3a450599907b661ae012233
ecandotti <enzo.candotti16@hotmail.com>
2022-01-08 16:35:07 +0100

    isAlreadyAuth verification for redirection to app

--- src/pages/Connexion.tsx
+++ src/pages/Connexion.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { auth } from '../utils/auth'
+import { urls } from '../utils/urls'
 
 import { Connexion as ConnexionLayout } from '../layouts/Connexion'
 
-export const Connexion: React.FC = () => (
-    <ConnexionLayout />
-)
+export const Connexion: React.FC = () => {
+    const navigate = useNavigate()
+
+    React.useEffect(() => {
+        if (auth.isAuthenticated) {
+            navigate(urls.app.PEARCLOUD, { replace: true })
+        }
+    }, [])
+
+    return (
+        <ConnexionLayout />
+    )
+}
