94007e36a0c016b02f309bb26a3e9d6deaff9c81
ecandotti <enzo.candotti16@hotmail.com>
2022-01-08 01:16:15 +0100

    update css and add Home layout (in progress)

--- src/pages/PearCloud.tsx
+++ src/pages/PearCloud.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 
 import { Navbar } from '../components/Navbar'
+import { Home } from '../layouts/Home'
 
 export const PearCloud: React.FC = () => {
     return (
-        <div className="bg-cyan-100 dark:bg-cyan-800">
+        <div className="flex w-full h-full bg-slate-100 dark:bg-slate-800 transition-colors">
             <Navbar />
+            <Home />
         </div>
     )
 }
