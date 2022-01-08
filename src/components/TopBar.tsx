3c37bdbd96568a1c2acf9ee599d88eed350fff3e
ecandotti <enzo.candotti16@hotmail.com>
2022-01-08 17:38:46 +0100

    update topBar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { GiPear } from 'react-icons/gi'
-import { GoSettings } from 'react-icons/go'
 
+import { MobileMenu } from './MobileMenu'
 import { DarkModeButton } from './DarkModeButton'
 
 export const TopBar: React.FC = () => {
     return (
-        <div className="relative md:hidden text-xl text-slate-800 dark:text-slate-200 bg-slate-50 dark:bg-slate-700 p-2">
-            <div className="absolute left-2 top-4">
-                <GoSettings className="mx-2" />
-            </div>
+        <div className="relative z-40 md:hidden text-xl text-slate-800 dark:text-slate-200 bg-slate-50 dark:bg-slate-700 p-2">
+            <MobileMenu />
             <div className="flex justify-center items-center text-3xl text-slate-800 dark:text-slate-200 select-none">
                 <span className="text-green-500">Pear</span>Cloud <GiPear />
             </div>
