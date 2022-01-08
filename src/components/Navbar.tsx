840c295bf1748fda493681117df6849cd3c8f2f6
ecandotti <enzo.candotti16@hotmail.com>
2022-01-08 16:35:47 +0100

    add mobile version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,37 +36,37 @@ export const Navbar: React.FC = () => {
     }
 
     return (
-        <nav className="flex flex-col items-center lg:items-stretch justify-between bg-slate-50 dark:bg-slate-700 p-1 py-3 lg:p-5">
-            <div className="flex justify-center items-center mb-10 text-3xl text-slate-800 dark:text-slate-200 select-none">
+        <nav className="flex flex-row md:flex-col items-center lg:items-stretch justify-between bg-slate-50 dark:bg-slate-700 p-0 md:p-1 md:py-3 lg:p-5">
+            <div className="hidden md:flex justify-center items-center mb-10 text-3xl text-slate-800 dark:text-slate-200 select-none">
                 <span className="hidden lg:inline"><span className="text-green-500">Pear</span>Cloud</span> <GiPear />
             </div>
-            <div>
-                <div className="my-5">
+            <div className="text-xl md:text-base flex flex-row justify-between w-full md:w-auto p-2 md:block">
+                <div className="my-2 md:my-5">
                     <Link to={urls.app.PEARCLOUD} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <FiHome className="mx-2"/> <span className="hidden lg:inline">Photos</span>
                     </Link>
                 </div>
-                <div className="my-5">
+                <div className="my-2 md:my-5">
                     <Link to={urls.app.IMPORT} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <AiOutlineCloud className="mx-2" /> <span className="hidden lg:inline">Importer</span>
                     </Link>
                 </div>
-                <div className="my-5">
+                <div className="my-2 md:my-5">
                     <Link to={urls.app.ALBUMS} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <BsJournalAlbum className="mx-2" /> <span className="hidden lg:inline">Albums</span>
                     </Link>
                 </div>
-                <div className="my-5">
+                <div className="my-2 md:my-5">
                     <Link to={urls.app.FAVORITES} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <FaRegStar className="mx-2" /> <span className="hidden lg:inline">Favoris</span>
                     </Link>
                 </div>
-                <div className="my-5">
+                <div className="my-2 md:my-5">
                     <Link to={urls.app.TRASH} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <BsTrash2 className="mx-2" /> <span className="hidden lg:inline">Corbeille</span>
                     </Link>
                 </div>
-                <div className="my-5">
+                <div className="hidden md:block my-2 md:my-5">
                     <Link to={urls.app.SETTINGS} className="flex items-center p-2 border rounded-xl text-slate-800 dark:text-slate-200 hover:border-green-500 hover:text-green-500 dark:hover:text-green-500 transition-colors">
                         <GoSettings className="mx-2" /> <span className="hidden lg:inline">Paramètre</span>
                     </Link>
@@ -77,15 +77,18 @@ export const Navbar: React.FC = () => {
                 <StorageInfo />
                 <DarkModeButton />
             </div>
-            <div className="lg:hidden flex items-center justify-between px-2 w-full">
+            <div className="hidden md:flex lg:hidden items-center justify-between px-2 w-full">
                 <LanguageButton />
                 <DarkModeButton />
             </div>
-            <div className="lg:hidden">
+            <div className="hidden md:block lg:hidden">
                 <StorageInfo />
             </div>
-            <div className="flex items-center justify-center lg:justify-between">
-                <div className="hidden lg:inline">
+            <div className="hidden md:flex items-center justify-between">
+                <div className="shrink-0 mr-4">
+                    <img className="h-10 w-10 object-cover rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="Current profile photo" />
+                </div>
+                <div className="hidden lg:inline mr-4">
                     <div className="text-xs text-gray-500 dark:text-gray-200">Connecté en tant que</div>
                     <div className="text-sm text-slate-800 dark:text-slate-200">{`${firstName} ${lastName[0]}.`}</div>
                 </div>
--- src/pages/PearCloud.tsx
+++ src/pages/PearCloud.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
+import { Outlet } from 'react-router-dom'
 
 import { Navbar } from '../components/Navbar'
-import { Home } from '../layouts/Home'
+import { TopBar } from '../components/TopBar'
 
 export const PearCloud: React.FC = () => {
     return (
-        <div className="flex w-full h-full bg-slate-100 dark:bg-slate-800 transition-colors">
+        <div className="flex flex-col-reverse md:flex-row w-full h-full bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-200 transition-colors">
             <Navbar />
-            <Home />
+            <Outlet />
+            <TopBar />
         </div>
     )
 }
