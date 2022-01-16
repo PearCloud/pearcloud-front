2d336a3ee72207f11c8896488858fe087bb3e0c8
ecandotti <enzo.candotti16@hotmail.com>
2022-01-16 19:13:28 +0100

    Import feature

--- src/pages/Import.tsx
+++ src/pages/Import.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react'
+import ReactLoading from 'react-loading'
+
+import { ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import { Navbar } from '../components/Navbar'
 import { TopBar } from '../components/TopBar'
 
 import FileHandler from "../services/FileHandler"
+import { DragAndDrop } from '../components/DragAndDrop'
 
 export const Import: React.FC = () => {
-    const [selectedFiles, setSelectedFiles] = React.useState<any>(undefined)
+    const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+    const [files, setFiles] = React.useState<any[]>([])
+    const [listOfPhotos, setListOfPhotos] = React.useState<any[]>([])
     const [currentFile, setCurrentFile] = React.useState<undefined>(undefined)
     const [progress, setProgress] = React.useState<number>(0)
-    const [message, setMessage] = React.useState<string>("")
 
-    const selectFile = (files: FileList) => {
-        setSelectedFiles(files)
-    }
+    const isDarkModeActive = document.querySelector('html')?.classList.contains('dark') || false
 
     const upload = () => {
-        let currentFile = selectedFiles[0]
+        let currentFile = files[0]
 
+        setIsLoading(true)
         setProgress(0)
         setCurrentFile(currentFile)
 
@@ -24,35 +31,48 @@ export const Import: React.FC = () => {
             setProgress(Math.round((100 * event.loaded) / event.total))
         })
         .then((response) => {
-            setMessage(response.data.message)
+            setIsLoading(false)
+            toast.info(response.data.message)
             return FileHandler.getFiles()
         })
         .then((files) => {
             console.log(files.data)
         })
         .catch(() => {
+            setIsLoading(false)
             setProgress(0)
-            setMessage("Could not upload the file!")
+            toast.error("Impossible d'upload l'image !")
             setCurrentFile(undefined)
         })
 
-        setSelectedFiles(undefined)
+        setFiles([])
+    }
+
+    const onFileChange = (files: any[]) => {
+        console.log(files)
     }
 
     return (
-        <div className="flex-1 md:ml-24 lg:ml-80 mt-14 md:mt-0">
-            <h1 className="text-center font-bold text-2xl my-3">Importer</h1>
-            <label className="btn btn-default">
-                <input type="file" onChange={selectFile as any} />
-            </label>
-
-            <button className="btn btn-success" disabled={!selectedFiles} onClick={upload}>
-                Upload
-            </button>
-
-            <div className="alert alert-light" role="alert">
-                {message}
+        <div className="flex flex-col-reverse md:flex-row w-full h-full bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-200 transition-colors">
+            <Navbar />
+            <div className="flex-1 flex flex-col items-center md:ml-24 lg:ml-80 mt-14 md:mt-0 mb-24 md:mb-0">
+                <DragAndDrop onFileChange={(files: any) => onFileChange(files)} />
+                <button
+                    onClick={upload}
+                    disabled={!files}
+                    className="mt-auto rounded-2xl py-4 px-8 my-4 bg-green-500 dark:bg-green-800 text-slate-100 dark:text-slate-200 cursor-pointer text-bold text-xl disabled:bg-slate-500 dark:disabled:bg-slate-500 disabled:cursor-not-allowed transition">
+                    {isLoading ? 
+                        <ReactLoading
+                            type="spin"
+                            color="red"
+                            height={17}
+                            width={17}
+                        /> : "Upload"
+                    }
+                </button>
             </div>
+            <TopBar />
+            <ToastContainer theme={`${isDarkModeActive ? 'dark' : 'light'}`}/>
         </div>
     )
 }
