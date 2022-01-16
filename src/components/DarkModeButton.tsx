51c08f900053e45a557d71ecd9a1991c25d2c249
ecandotti <enzo.candotti16@hotmail.com>
2022-01-16 02:41:55 +0100

    update bg-color status bar for iphone

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -7,9 +7,13 @@ export const DarkModeButton: React.FC = () => {
     const darkMode = () => {
         if (document.querySelector('html')?.classList.contains('dark')) {
             document.querySelector('html')?.classList.remove('dark')
+            document.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#f8fafc");
+            document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]')?.setAttribute("content", "#f8fafc");
             setThemeDark(false)
         } else {
             document.querySelector('html')?.classList.add('dark')
+            document.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#334155");
+            document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]')?.setAttribute("content", "#334155");
             setThemeDark(true)
         }
     }
