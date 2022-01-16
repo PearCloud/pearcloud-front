37b8fca24dfeaf2c78e0e5c21211c6740aa73c02
ecandotti <enzo.candotti16@hotmail.com>
2022-01-16 02:43:49 +0100

    prepare for getting photos

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react'
 
+import FileHandler from '../services/FileHandler'
+
 export const Home: React.FC = () => {
+    const [fileInfos, setFileInfos] = React.useState<any>([])
+
+    // React.useEffect(() => {
+    //     FileHandler.getFiles().then((res) => {
+    //         setFileInfos(res.data)
+    //     })
+    // }, [])
+
     return (
-        <h1 className="flex-1">Home App</h1>
+        <h1 className="flex-1 md:ml-24 lg:ml-80 mt-14 md:mt-0">Home App</h1>
     )
 }
